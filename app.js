6e4edc99e9b903a071a723003d35d25ee189081b
María Julia Quilis <quilismariajulia@hotmail.com>
2025-01-29 21:44:02 -0300

    Avancé con el challenge

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Declaro una variable de tipo array, que almacenará los nombres de los amigos ingresados.
+let amigos = [];
+
+//Implemento dos funciones: una para agregar amigos al array "amigos" y otra para limpiar el campo de entrada
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById('amigo').value;
+    if(nombreAmigo === ''){
+        alert("Por favor, inserte un nombre");
+    }else{
+        amigos.push(nombreAmigo);
+        //console.log(amigos);
+        limpioCampoDeEntrada();
+    }
+}
+
+function limpioCampoDeEntrada(){
+    document.querySelector('#amigo').value = '';
+}
