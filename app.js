ab5f0259b0030efe5da08f2af4960df337498bad
María Julia Quilis <quilismariajulia@hotmail.com>
2025-02-01 01:29:13 -0300

    Terminé el challenge

--- app.js
+++ app.js
@@ -10,9 +10,32 @@ function agregarAmigo(){
         amigos.push(nombreAmigo);
         //console.log(amigos);
         limpioCampoDeEntrada();
+        actualizarLista();
     }
 }
 
 function limpioCampoDeEntrada(){
     document.querySelector('#amigo').value = '';
+}
+
+function actualizarLista(){
+    let lista = document.getElementById('listaAmigos'); //selecciono la lista donde se mostrarán los amigos
+    lista.innerHTML = ""; //limpio la lista
+    for(let i = 0; i < amigos.length; i++){
+        let li = document.createElement('li'); //creo el elemento de lista (<li>)
+        li.textContent = amigos[i]; 
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo(){
+    if(amigos.length == 0){
+        alert("El arreglo no tiene elementos");
+    }else{
+        let indiceAleatorio = Math.floor(Math.random()*amigos.length);
+        //console.log(indiceAleatorio);
+        let amigoSorteado = amigos[indiceAleatorio];
+        let muestroAmigoSorteado = document.getElementById('resultado');
+        muestroAmigoSorteado.innerHTML = "El amigo secreto es " + amigoSorteado;
+    }
 }
