73170067461a73c96baf5460c52cc570bad88c45
María Julia Quilis <quilismariajulia@hotmail.com>
2025-02-01 03:09:14 -0300

    Agrego un par de comentarios

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ let amigos = [];
 
 //Implemento dos funciones: una para agregar amigos al array "amigos" y otra para limpiar el campo de entrada
 function agregarAmigo(){
+    //Capturo el valor del campo de entrada.
     let nombreAmigo = document.getElementById('amigo').value;
+    //Valido la entrada para asegurarme de que el campo no esté vacío.
     if(nombreAmigo === ''){
         alert("Por favor, inserte un nombre");
     }else{
+        //añado el nombre del amigo al arreglo
         amigos.push(nombreAmigo);
-        //console.log(amigos);
         limpioCampoDeEntrada();
         actualizarLista();
     }
@@ -18,6 +20,7 @@ function limpioCampoDeEntrada(){
     document.querySelector('#amigo').value = '';
 }
 
+//actualizarLista() agrega a una lista HTML los nombres que hay en el arreglo.
 function actualizarLista(){
     let lista = document.getElementById('listaAmigos'); //selecciono la lista donde se mostrarán los amigos
     lista.innerHTML = ""; //limpio la lista
@@ -28,12 +31,12 @@ function actualizarLista(){
     }
 }
 
+//sortearAmigo() selecciona de manera aleatoria uno de los nombres almacenados en el array.
 function sortearAmigo(){
     if(amigos.length == 0){
-        alert("El arreglo no tiene elementos");
+        alert("No hay amigos para sortear :(");
     }else{
         let indiceAleatorio = Math.floor(Math.random()*amigos.length);
-        //console.log(indiceAleatorio);
         let amigoSorteado = amigos[indiceAleatorio];
         let muestroAmigoSorteado = document.getElementById('resultado');
         muestroAmigoSorteado.innerHTML = "El amigo secreto es " + amigoSorteado;
